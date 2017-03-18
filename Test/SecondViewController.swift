555b4a04cefd41d2cd82ed2c3cb7f9d92f9e2709
Anh Duc Tran <anhduc.t92@gmail.com>
2017-03-18 15:20:15 +0100

    Initial Commit

---
+++ Test/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Test
+//
+//  Created by Anh Duc Tran on 18.03.17.
+//  Copyright © 2017 Anh Duc Tran. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
